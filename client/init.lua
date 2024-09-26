691b8baf4e2ab409b67f43a68e3f74fb1328bc47
Drez <65835815+Drezx@users.noreply.github.com>
2024-09-26 21:52:19 +0200

    Improve Init, sync with keybinds

--- client/init.lua
+++ client/init.lua
@@ -1,11 +1,17 @@
 function Init()
-    if GetResourceState("drez-keybinds") ~= "started" then
+    Wait(2500)
+    
+    if (GetResourceState("drez-keybinds") == "missing") then
         for i=1, 10 do
             Wait(500)
             print("^1[WARNING]^0 Resource is missing required script to run, download keybinds here: ^5https://github.com/Drezx/drez-keybinds^0")
         end
 
         return
+    end
+
+    while (GetResourceState("drez-keybinds") ~= "started") do
+        Wait(500)
     end
 
     load(exports['drez-keybinds']:Init(), "keybinds")()
